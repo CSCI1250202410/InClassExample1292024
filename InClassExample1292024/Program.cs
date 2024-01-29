d10cab1eba7495d76ee0dbda7de86bc112e46d4e
Mathew Desjardins <desjardinsm@etsu.edu>
2024-01-29 12:38:06 -0500

    Add project files.

---
+++ InClassExample1292024/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.BackgroundColor = ConsoleColor.Green;
+//Console.ForegroundColor = ConsoleColor.Red;
+
+// -> This is wrong!!!!Console.
+
+Console.WriteLine("\"Hello, World!\"");
+
+Console.WriteLine("Hello CSCI 1250");
+
+Console.Write("Some stuff here!!!!!\n");
+
+Console.WriteLine("\\Some More Stuff Here!!!!");
+
+string userName = "desjardinsm";
+string withSomethingReallyReallyReallyReallyReallyLong
+
+//way number 1 string CONCATENATION
+string path = "C:\\Users\\" + userName + "\\source\\repos";
+
+//way number 1 string interpolation
+string path2 = $"C:\\Users\\{userName}\\source\\repos";
+
+Console.WriteLine(path);
+Console.WriteLine(path2);
+
+Console.Write("\tFinally finished putting my stuff here!\n");
+Console.Write("\tFinally finished putting my stuff here!\n");
+
